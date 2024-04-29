6710cdc08f10d0d0296bc3e7adcbbf09f019aa43
Christian Campos <christiancampos9559@gmail.com>
2024-04-29 12:31:51 -0300

    Segundo commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade = 18;
+        char sexo = 'M';
+        String nome = "Christian";
+        float altura = 1.76F;
+        double salario = 3500.76;
     }
 }
